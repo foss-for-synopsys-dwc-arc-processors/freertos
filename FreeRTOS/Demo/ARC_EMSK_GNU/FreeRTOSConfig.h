f172ef7854e08e7e5f403d184ec10f35a373ca12
Huaqi Fang <Huaqi.Fang@synopsys.com>
2017-03-31 15:12:44 +0800

    Enable FreeRTOS debug for mdb

--- FreeRTOS/Demo/ARC_EMSK_GNU/FreeRTOSConfig.h
+++ FreeRTOS/Demo/ARC_EMSK_GNU/FreeRTOSConfig.h
@@ -95,6 +95,7 @@
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
 #define configQUEUE_REGISTRY_SIZE	0
+#define configUSE_MUTEXES           1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0

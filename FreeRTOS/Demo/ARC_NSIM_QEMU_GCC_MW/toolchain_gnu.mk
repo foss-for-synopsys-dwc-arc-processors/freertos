a68271f9ad4d0e541d6bc02d459627c1a9a3a42e
apower-syn <apower@synopsys.com>
2023-12-06 16:37:42 +0000

    Updating makefiles to support HS6x on nsim, and restoring ARC Demo makefiles

--- FreeRTOS/Demo/ARC_NSIM_QEMU_GCC_MW/toolchain_gnu.mk
+++ FreeRTOS/Demo/ARC_NSIM_QEMU_GCC_MW/toolchain_gnu.mk
@@ -20,11 +20,23 @@ endif
 
 TOOLCHAIN_DEFINES += $(HEAP_DEFINES) $(STACK_DEFINES) -D__GNU__
 
-NSIMDRV_OPT = -propsfile nsim.props
+ifeq ($(ARC_CPU), hs5x)
+	NSIMDRV_OPT = -propsfile nsim_hs5x.props
+else
+  ## ARC_CPU == arcem
+	NSIMDRV_OPT = -propsfile nsim.props
+endif
 
 DBG_HW_FLAGS += -ex "target remote localhost:1234" -ex "load"
 
-CCORE_OPT_GNU += -mcpu=hs6x -mcode-density -mdiv-rem --param l1-cache-size=16384 --param l1-cache-line-size=32
+ifeq ($(ARC_CPU), hs5x)
+	CCORE_OPT_GNU += -mcpu=hs5x -mcode-density -mdiv-rem \
+		--param l1-cache-size=16384 --param l1-cache-line-size=32
+else
+  ## ARC_CPU == arcem
+	CCORE_OPT_GNU += -mcpu=em4_dmips -mlittle-endian -mcode-density -mdiv-rem -mswap -mnorm \
+		-mmpy-option=6 -mbarrel-shifter --param l1-cache-size=16384 --param l1-cache-line-size=32
+endif
 
 ACORE_OPT_GNU += $(CCORE_OPT_GNU)
 
@@ -39,11 +51,18 @@ GNU_TOOLCHAIN_PREFIX := $(wildcard $(GNU_TOOLCHAIN_PREFIX))
 endif
 
 ## GNU TOOLCHAIN TOOLS NAME DEFINITIONS ##
-	CC  = arc64-elf-gcc
+ifeq ($(ARC_CPU), hs5x)
+	CC	= arc64-elf-gcc
 	LD	= arc64-elf-gcc
+	DBG	?= arc64-elf-gdb
+else
+  ## ARC_CPU == arcem
+	CC	= arc-elf32-gcc
+	LD	= arc-elf32-gcc
+	DBG	?= arc-elf32-gdb
+endif
 
 	MAKE	= make
-	DBG	?= arc64-elf-gdb
 
 	NSIMDRV	= nsimdrv
 
@@ -63,13 +82,28 @@ endif
 ##
 	## Common Options
 	MKDEP_OPT	= -MMD -MT $@ -MF $@.d
+
+ifeq ($(ARC_CPU), hs5x)
 	COMMON_COMPILE_OPT = -ffunction-sections -fdata-sections \
-			$(OPT_OLEVEL) $(CDEBUG_OPTION) $(ALL_DEFINES) $(ALL_INCLUDES) $(MKDEP_OPT)
+		$(OPT_OLEVEL) $(CDEBUG_OPTION) $(ALL_DEFINES) $(ALL_INCLUDES) $(MKDEP_OPT)
+else
+  ## ARC_CPU == arcem
+	COMMON_COMPILE_OPT = -ffunction-sections -fdata-sections -mno-sdata \
+		$(OPT_OLEVEL) $(CDEBUG_OPTION) $(ALL_DEFINES) $(ALL_INCLUDES) $(MKDEP_OPT)
+endif
 
 	## C/ASM/LINK Options
 	COMPILE_OPT	+= $(CCORE_OPT_GNU)   $(ADT_COPT)   $(COMMON_COMPILE_OPT) -std=gnu99
 	ASM_OPT		+= $(ACORE_OPT_GNU)   $(ADT_AOPT)   $(COMMON_COMPILE_OPT) -x assembler-with-cpp
-	LINK_OPT	+= $(LCORE_OPT_GNU)   $(ADT_LOPT) -Wl,--gc-sections -nostartfiles $(LMAP_OPTION) -lm -Wl,--script=$(APPL_LINK_FILE)
+
+ifeq ($(ARC_CPU), hs5x)
+	LINK_OPT	+= $(LCORE_OPT_GNU)   $(ADT_LOPT) -Wl,--gc-sections \
+		-nostartfiles $(LMAP_OPTION) -lm -Wl,--script=$(APPL_LINK_FILE)
+else
+  ## ARC_CPU == arcem
+	LINK_OPT	+= $(LCORE_OPT_GNU)   $(ADT_LOPT) -Wl,--gc-sections \
+		-mno-sdata -nostartfiles $(LMAP_OPTION) -lm -Wl,--script=$(APPL_LINK_FILE)
+endif
 
 	## Link File Generation Options
 	LINK_FILE_OPT	+= -x assembler-with-cpp $(ALL_INCLUDES) -E -P -nostdinc -undef -D__GNU__
@@ -78,4 +112,3 @@ endif
 	LD_START_GROUPLIB	= -Wl,--start-group
 	LD_SYSTEMLIBS		= -lm -lc -lgcc
 	LD_END_GROUPLIB		= -Wl,--end-group
-	
